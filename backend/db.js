67d5284b109f09b014654ccb6800b75d41ec24c2
Nagarjun <nagarjun3147@gmail.com>
2026-07-10 22:52:05 +0530

    mongodb+srv://nagarjun:Nagarjun12345@operationsdashboard.ztcgudm.mongodb.net/operations_dashboard?retryWrites=true&w=majority&appName=OperationsDashboard

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
-    console.log("MONGO_URI =", process.env.MONGO_URI?.replace(/:(.*?)@/, ":********@"));
+    console.log("MONGODB_URI =", process.env.MONGODB_URI?.replace(/:(.*?@)/, ":********@"));
 
-    await mongoose.connect(process.env.MONGO_URI);
+await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("✅ MongoDB Connected");
   } catch (err) {
